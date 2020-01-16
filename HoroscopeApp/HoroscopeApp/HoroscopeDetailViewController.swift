1ebb9ba5633d64eb8705c3b6be37020a228e6fd2
Yuliia Engman <yuliia.engman@gmail.com>
2020-01-16 07:34:11 -0500

    text field in detail VC works

--- HoroscopeApp/HoroscopeApp/HoroscopeDetailViewController.swift
+++ HoroscopeApp/HoroscopeApp/HoroscopeDetailViewController.swift
@@ -14,6 +14,9 @@ class HoroscopeDetailViewController: UITableViewController {
     @IBOutlet weak var horoscopePicker: UIPickerView!
     @IBOutlet weak var datePicker: UIDatePicker!
     
+    @IBOutlet var horoscopeButtonLabel: UITableView!
+    
+    
     // data for our picker view - ask how we could populate in from API
     private let horoscopes = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"]
 
@@ -24,17 +27,20 @@ class HoroscopeDetailViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // we have to configure the picker view
         horoscopePicker.dataSource = self
         horoscopePicker.delegate = self
-        
-        //horoscope?.sunsign.first
        signName = horoscopes.first
+        
+        nameTextField.delegate = self
     }
 
     // MARK: - Table view data source
 
+    
+    @IBAction func showHoroscopeActionButton(_ sender: UIButton) {
+    }
+    
+    
 }
 
 extension HoroscopeDetailViewController: UIPickerViewDataSource {
@@ -44,7 +50,6 @@ extension HoroscopeDetailViewController: UIPickerViewDataSource {
 
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return horoscopes.count
-        //return horoscope?.sunsign.count ?? 1
     }
 }
 
@@ -52,5 +57,17 @@ extension HoroscopeDetailViewController: UIPickerViewDelegate {
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return horoscopes[row]
     }
+}
+
+extension HoroscopeDetailViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+           
+           //dismiss the keyboard
+           textField.resignFirstResponder()
+           
+           // update name of event
+//           event?.name = textField.text ?? "no event name"
+           return true // true or false does not affect
+        }
 }
 
